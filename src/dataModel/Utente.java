838b424e0892ffe3d57eb784f3c7b4413d4c06b7
Mehdi Amlal <mehdi.amlal.business@gmail.com>
2022-10-19 21:50:18 +0200

    modificata classe Utente, con aggiunta nome e cognome

--- src/dataModel/Utente.java
+++ src/dataModel/Utente.java
@@ -3,31 +3,45 @@ package dataModel;
 public class Utente {
 
     private String username;
+    private String nome;
+    private String cognome;
     private String password;
     private String ruolo;
 
-    public Utente(String nomeAccount, String password, String ruolo) {
-        this.username = nomeAccount;
+    public Utente(String username, String nome, String cognome, String password, String ruolo) {
+        this.username = username;
+        this.nome = nome;
+        this.cognome = cognome;
         this.password = password;
         this.ruolo = ruolo;
     }
 
     public String getUsername() {
-        return this.username;
+        return username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
     }
 
     public String getPassword() {
-        return this.password;
+        return password;
     }
 
     public String getRuolo() {
-        return this.ruolo;
+        return ruolo;
     }
 
     @Override
     public String toString() {
         return "Utente{" +
                 "username='" + username + '\'' +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
                 ", password='" + password + '\'' +
                 ", ruolo='" + ruolo + '\'' +
                 '}';
