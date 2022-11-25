057a6f109e012dbc54c1fb888fa1e9672c0bbd01
Amazeeing <fplfrancesco@gmail.com>
2022-11-25 19:31:38 +0100

    Aggiunta component Navigation Bar

---
+++ src/main/webapp/js/components/navbar.js
@@ -0,0 +1,11 @@
+export var navigationBar = {
+    template: `
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <div class="navbar-nav">
+            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="#">Prenotazioni</a>
+            <a class="nav-link" href="#">Corsi</a>
+        </div>
+    </nav>
+    `
+}
