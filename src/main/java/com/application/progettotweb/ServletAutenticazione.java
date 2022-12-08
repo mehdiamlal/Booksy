3c6928f887b481c6cb3b756a9fc572c9ed845391
Amazeeing <fplfrancesco@gmail.com>
2022-12-08 12:50:19 +0100

    Corretto nome servlet autenticazione

--- src/main/java/com/application/progettotweb/ServletAutenticazione.java
+++ src/main/java/com/application/progettotweb/ServletAutenticazione.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "servletCorso", value = "/servlet-corso")
+@WebServlet(name = "servletAutenticazione", value = "/servlet-autenticazione")
 public class ServletAutenticazione extends HttpServlet {
     DAO dataModel;
 
